5d3c7c5b4934c8ae3fd5dd78c7daed246bdc48bb
Devadath K S <119772389+Harmless04@users.noreply.github.com>
2023-10-17 19:32:54 +0530

    Update blackjack.js
    added save button and new card button also saves the old cards

--- blackjack.js
+++ blackjack.js
@@ -1,25 +1,44 @@
 let firstCard = 10
 let secondCard = 11
-let sum=firstCard + secondCard 
+let cards = [firstCard,secondCard]
+let sum=firstCard + secondCard+newCard
 let hasBlackjack = false
 let isAlive = true
 let message = ""
+let messageEl=document.getElementById("message-el")
+let sumEl=document.getElementById("sum-el") 
+let cardsEl=document.getElementById("cards-el")
+
 function startGame(){
-if(sum<=20) 
-{
-  message= "do you want to draw a new card?😍"
+  renderGame()
 }
+
+function renderGame(){
+  cardsEl.textContent="cards:"+ cards[0] + " "+ cards[1]
+  sumEl.textContent= "sum:"+sum
+if(sum<=20)  
+{
+  message= "do you want to draw a new card?"
+}
+
 else if (sum==21)
 {
-  message="you've got blackjack!!😁🙌"
+  message="you've got blackjack!!"
   hasBlackjack = true
 }
 else{
-  message="you lost🤦‍♂️😢!!"
+  message="you lost!!"
   isAlive = false
 }
 
-console.log(message)
+messageEl.textContent = message
 }
+function newCard(){
+  console.log("Drawing a new card from the deck")
+  let Card = 7
+  sum += card
+  renderGame()
+}
+
 
 
