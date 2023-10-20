6f765a91e5be39fe1f79d10dcf04ea1bcc3db98c
Devadath K S <119772389+Harmless04@users.noreply.github.com>
2023-10-20 08:58:36 +0530

    Update blackjack.js
    random cards now generated instead of hardcoding and no sum defined before the game starts

--- blackjack.js
+++ blackjack.js
@@ -1,20 +1,42 @@
-let firstCard = 10
-let secondCard = 11
-let cards = [firstCard,secondCard]
-let sum=firstCard + secondCard+newCard
+let cards = []
+let sum=0
 let hasBlackjack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl=document.getElementById("message-el")
 let sumEl=document.getElementById("sum-el") 
 let cardsEl=document.getElementById("cards-el")
 
+function getRandomCard(){
+  
+  let randomNumber = Math.floor(Math.random()*13)+1
+  if(randomNumber>10){
+    return 10
+  }
+  else  if(randomNumber===1){
+    return 11
+  }
+  else{
+    return randomNumber
+  }
+  
+}
+
 function startGame(){
+  isAlive=true
+  let firstCard = getRandomCard()
+  let secondCard = getRandomCard()
+  cards=[firstCard ,secondCard]
+  sum=firstCard+secondCard
   renderGame()
 }
 
 function renderGame(){
-  cardsEl.textContent="cards:"+ cards[0] + " "+ cards[1]
+  cardsEl.textContent="cards:"
+  for(let i= 0;i<cards.length;i++)
+  {
+      cardsEl.textContent += cards[i]+" "
+  }
   sumEl.textContent= "sum:"+sum
 if(sum<=20)  
 {
@@ -35,10 +57,14 @@ messageEl.textContent = message
 }
 function newCard(){
   console.log("Drawing a new card from the deck")
-  let Card = 7
+  let card = getRandomCard()
   sum += card
+  cards.push(card)
+  console.log(cards)
   renderGame()
 }
+
+
 
 
 
